dde4b346cb09b6ea6007b069c71d658a1fa07b95
natashalukmanto <natshaaudrey@gmail.com>
2026-03-13 22:24:02 -0700

    feat: 04. Build a Blackjack Game

--- blackjack/index.js
+++ blackjack/index.js
@@ -1,23 +1,74 @@
-let firstCard = 10
-let secondCard = 9
-let sum = firstCard + secondCard
+let sum = 0
+let cards = []
 let blackjack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+let player = {
+    name: "Per",
+    chips: 145, 
+    sayHello: function() {
+        console.log("Hello, " + player.name)
+    }
+}
+
+player.sayHello()
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
+function getRandomCard() {
+    r = Math.floor(Math.random() * 13) + 2
+
+    if (r === 1) {
+        return 1
+    } else if (r > 10) {
+        return 10
+    } else {
+        return r
+    }
+}
 
 function startGame() {
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    isAlive = true
+    renderGame()
+}
+
+function renderGame() {
     if (sum < 21) {
     message = "Do you want to draw a new card? :)"
     } else if (sum === 21) {
         message = "You have won! :D"
         blackjack = true
+        isAlive = false
     } else {
         message = "You're out of the game. :("
         isAlive = false
     }
 
     messageEl.textContent = message
+    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = "Cards: " 
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + ' '
+    }
+    
+}
+
+function newCard() {
+    if (isAlive && !blackjack) {
+        let new_card = getRandomCard()
+        sum += new_card
+        cards.push(new_card)
+        renderGame()
+    }
     
 }
 
