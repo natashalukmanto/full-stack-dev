44c6d07995b9429e3a02b1bc1e752ec09f1fafd4
natashalukmanto <natshaaudrey@gmail.com>
2026-03-17 11:37:38 -0700

    feat: 02. if else

--- objects&func/index.js
+++ objects&func/index.js
@@ -1,17 +1,22 @@
-// Create a person object that contains three keys: name, age, and county.
-// Use yourself as an example to set the values for name, age, and country
-let person = {
-    name: "Natasha", 
-    age: 21,
-    country: "Indonesia"
-}
+let age = 15
 
-// Create a function, logData(), that uses the person object to create a
-// string in the following format: 
-// "Per is 35 years old and lives in Norway"
-function logData() {
-    console.log(person.name + " is " + person.age + " years old and lives in " + person.country)
-}
+// less than 6 years old -> free
+// 6 to 17 years old     -> child discount
+// 18 to 26 years old    -> student discount
+// 27 to 66 years old    -> full price
+// over 66 years old     -> senior citizen discount
 
-// Call the logData() function to verify that it works
-logData()
+// Create a conditional statement (if/else/else if) that logs out the discount
+// the passenger will get based upon the value of the age variable
+
+if (age < 6) {
+    console.log("free")
+} else if (age < 18) {
+    console.log("child discount")
+} else if (age < 27) {
+    console.log("student discount")
+} else if (age < 67) {
+    console.log("full price")
+} else {
+    console.log("senior citizen discount")
+}
