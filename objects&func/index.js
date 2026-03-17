a9355470e401c72609b91fd4b2b8738ffa7cee79
natashalukmanto <natshaaudrey@gmail.com>
2026-03-17 12:30:57 -0700

    feat: 07. EmojiFighter

--- objects&func/index.js
+++ objects&func/index.js
@@ -1,9 +1,14 @@
-let hands = ["rock", "paper", "scissor"]
+let fighters = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷","🐆", "🦕", "🦁"]
 
-// Create a function that returns a random item from the array
+let stageEl = document.getElementById("stage")
+let fightButton = document.getElementById("fightButton")
 
-function randomHands() {
-    return hands[Math.floor(Math.random() * 3)];
-}
-
-console.log(randomHands())
+fightButton.addEventListener("click", function() {
+    // Challenge:
+    // When the user clicks on the "Pick Fighters" button, pick two random 
+    // emoji fighters and display them as i.e. "🦀 vs 🐢" in the "stage" <div>.
+    let r1 = Math.floor(Math.random() * fighters.length)
+    let r2 = Math.floor(Math.random() * fighters.length)
+    console.log(r1, r2)
+    stageEl.textContent = fighters[r1] + ' vs ' + fighters[r2]
+})
