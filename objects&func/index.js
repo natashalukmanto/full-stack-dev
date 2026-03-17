6cac958e250ab21adb1d886b91797fcbd11d1533
natashalukmanto <natshaaudrey@gmail.com>
2026-03-17 12:24:21 -0700

    feat: 06. Rock papers scissors

--- objects&func/index.js
+++ objects&func/index.js
@@ -1,9 +1,9 @@
-let dayOfMonth = 13
-let weekday = "Friday"
+let hands = ["rock", "paper", "scissor"]
 
-// If it is Friday the 13th, log out this spooky face: 😱
-// Use the logical "AND operator" -> &&
+// Create a function that returns a random item from the array
 
-if (dayOfMonth === 13 && weekday === "Friday") {
-    console.log("😱")
+function randomHands() {
+    return hands[Math.floor(Math.random() * 3)];
 }
+
+console.log(randomHands())
