11b45e49b2259571a71b5a39f9d3e34fed3e82d8
natashalukmanto <natshaaudrey@gmail.com>
2026-04-23 11:01:47 -0700

    fix: fix localStorage behavior

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -3,6 +3,8 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+renderLeads()
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
@@ -15,3 +17,14 @@ inputBtn.addEventListener("click", function() {
     `
     inputEl.value = ""
 })
+
+function renderLeads() {
+    for (let i = 0; i < myLeads.length; i++) {
+        ulEl.innerHTML += 
+        `<li>
+            <a href="${myLeads[i]}" target="_blank">
+                ${myLeads[i]}
+            </a>
+        </li>`
+    }
+}
