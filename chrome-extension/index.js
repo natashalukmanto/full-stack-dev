2edc6f9074f1146dee4bc9777d00740150ab4105
natashalukmanto <natshaaudrey@gmail.com>
2026-04-24 15:42:17 -0700

    nit: cleanup

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -4,32 +4,11 @@ const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
 
-renderLeads()
-console.log(deleteBtn)
+render(myLeads)
 
-deleteBtn.addEventListener("dblclick", function() {
-    console.log("double clicked!")
-    localStorage.clear()
-    myLeads = []
-    renderLeads()
-})
-
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    ulEl.innerHTML += `
-        <li>
-            <a href="${inputEl.value}" target="_blank">
-                ${inputEl.value}
-            </a>
-        </li>
-    `
-    inputEl.value = ""
-})
-
-function renderLeads() {
+function render(leads) {
     ulEl.innerHTML = ''
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         ulEl.innerHTML += 
         `<li>
             <a href="${myLeads[i]}" target="_blank">
@@ -39,7 +18,15 @@ function renderLeads() {
     }
 }
 
-function deleteLeads() {
+deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear()
-    renderLeads()
-}
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+})
